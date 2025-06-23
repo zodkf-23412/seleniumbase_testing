37d22cc9ea3751ac28625d33d1625db9434add10
David Li <davidli012345@gmail.com>
2025-06-23 12:00:31 -0700

    Update raw_recaptcha.py

--- examples/raw_recaptcha.py
+++ examples/raw_recaptcha.py
@@ -1,6 +1,8 @@
 from seleniumbase import SB
-from pyvirtualdisplay import Display
-with SB(uc=True, test=True,  xvfb=True) as sb:
+from pyvirtualdisplay import Display=
+display = Display(visible=0, size=(1024, 768))
+display.start()
+with SB(uc=True, test=True) as sb:
     url = "https://seleniumbase.io/apps/recaptcha"
     sb.activate_cdp_mode(url)
     sb.sleep(1)
